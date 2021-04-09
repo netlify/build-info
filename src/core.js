19c7ef2915a92b0eb22751e25f6252977137f427
João Antunes <joao@netlify.com>
2021-04-09 17:43:10 +0100

    chore: move workspaces logic (#32)

--- src/core.js
+++ src/core.js
@@ -1,28 +1,6 @@
 'use strict'
 
-const mapWorkspaces = require('@npmcli/map-workspaces')
-
-const getWorkspaceInfo = async function ({ rootPackageJson, projectDir, rootDir }) {
-  if (!rootPackageJson.workspaces) {
-    return
-  }
-
-  const workspacesMap = await mapWorkspaces({
-    cwd: rootDir || projectDir,
-    pkg: rootPackageJson,
-  })
-
-  const packages = [...workspacesMap.values()]
-  // The provided project dir is a workspace package
-  const isWorkspace = packages.find((path) => projectDir === path)
-
-  // The project dir is a collection of workspaces itself
-  const isRoot = !rootDir
-
-  if (isWorkspace || isRoot) {
-    return { isRoot, packages }
-  }
-}
+const { getWorkspaceInfo } = require('./workspaces')
 
 const buildInfo = async function (context) {
   const workspaceInfo = await getWorkspaceInfo(context)
